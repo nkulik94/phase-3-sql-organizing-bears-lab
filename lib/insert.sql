8b5f077d23554585779be4703ec7de01c629bdb7
Naftali Kulik <nkulik1994@gmail.com>
2022-06-26 10:38:37 -0400

    commit tree 39d5b65fc31c64449c70aae4da80d2762131f188

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "green", "annoying", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "orange", "happy", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "purple", "angry", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "yellow", "scary", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 90, "F", "black", "calm", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 150, "M", "green", "awesome", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 24, "F", "tangerine", "depressed", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 56, "M", "white", "unknown", true);
